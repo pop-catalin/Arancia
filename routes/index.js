c3bb48aee82180019004689091c0f4202fb0d1e6
pop-catalin <catalinpop98@gmail.com>
2020-09-03 20:10:09 +0300

    add and display task functionality completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.js');
+const Task = require('../models/task.js');
 
 router.get('/', checkAutheneticated, (req, res) => {
-    res.render('index.ejs', {name: req.user.name});
+    res.render('index.ejs', {tasks: req.user.tasks});
 });
+
+router.post('/addTask', (req, res) => {
+    User.findOne({email: req.user.email})
+        .then(async user => {
+            //console.log(user);
+            console.log(req.body.taskName);
+            const task = new Task.model({name: req.body.taskName});
+            user.tasks.addToSet(task);
+            console.log(user.tasks);
+            await user.save();
+            res.render('index.ejs', {tasks: user.tasks});
+        }).catch(err => console.log(err));
+})
 
 function checkAutheneticated(req, res, next) {
     if (req.isAuthenticated()) {
