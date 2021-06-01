4410d19adcc5e6999f59e481ff7f63911f7d0d6d
pop-catalin <catalinpop98@gmail.com>
2021-06-01 10:20:45 +0300

    timer bug on end of pause fix and style changes

--- public/ajaxscript.js
+++ public/ajaxscript.js
@@ -1,61 +1,70 @@
-$(function() {
+$(function () {
+	//POST
+	$('#addTask').on('submit', function (event) {
+		event.preventDefault();
+		var createInput = $('#taskName');
 
-    //POST
-    $("#addTask").on('submit', function(event){
-        event.preventDefault();
-        var createInput = $('#taskName');
+		$.ajax({
+			url: '/addTask',
+			method: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({ name: createInput.val() }),
+			success: function (data) {
+				addTaskVisualChanges(data);
+				createInput.val('');
+			},
+			error: function (e) {
+				alert(e.responseText);
+			},
+		});
+	});
 
-        $.ajax({
-            url: '/addTask',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ name: createInput.val() }),
-            success: function(data) {
-                addTaskVisualChanges(data);
-                createInput.val('');
-            },
-            error: function(e) {
-                alert(e.responseText);
-            }
-        });
-          
-        });
+	//DELETE
+	$('ul').on('click', '.deleteButton', function (event) {
+		event.preventDefault();
+		var form = $(this).closest('form');
+		var taskId = form.find('[name=itemId]').val();
 
-    //DELETE
-    $('ul').on('click', '.deleteButton', function(event) {
-        event.preventDefault();
-        var form = $(this).closest('form');
-        var taskId = form.find("[name=itemId]").val();
-
-        $.ajax({
-            url: '/deleteTask/' + taskId,
-            method: 'POST',
-            contentType: 'application/json',
-            success: function(data) {
-                deleteTaskVisualChanges(data, form);
-            }
-        })
-    })
-
+		$.ajax({
+			url: '/deleteTask/' + taskId,
+			method: 'POST',
+			contentType: 'application/json',
+			success: function (data) {
+				deleteTaskVisualChanges(data, form);
+			},
+		});
+	});
 });
 
 //remove the deleted task without needing to refresh(only the visual components)
 function deleteTaskVisualChanges(data, form) {
-    if(data.length > 6) {
-        const divHeight = parseInt($('.taskContainer').css("height").slice(0, -2)) - 35;
-        $('.taskContainer').css("height", divHeight + "px");
-    }
-    $('#' + data.id).remove();
-    $(form).remove();
+	if (data.length > 5) {
+		const divHeight =
+			parseInt($('.taskContainer').css('height').slice(0, -2)) - 40;
+		$('.taskContainer').css('height', divHeight + 'px');
+	}
+	$('#' + data.id).remove();
+	$(form).remove();
 }
 
 //show the added task without needing to refresh
 function addTaskVisualChanges(data) {
-    console.log();
-    if(data.length > 7) {
-        const divHeight = parseInt($('.taskContainer').css("height").slice(0, -2)) + 35;
-        $('.taskContainer').css("height", divHeight + "px");
-    }
-    $('#taskList').append("<li style=\"margin-right: 0.58em\" id="+ data.task._id +">" + data.task.name + "</li>");
-    $('#' + data.task._id).after("<form class=\"deleteForm\"> <input type=\"hidden\" name=\"itemId\" value=" + data.task._id + "> <button class=\"deleteButton\"><i class=\"fa fa-times\" style=\"font-size: 1.25em; \"></i> </button> </form>");                
+	console.log();
+	if (data.length > 6) {
+		const divHeight =
+			parseInt($('.taskContainer').css('height').slice(0, -2)) + 40;
+		$('.taskContainer').css('height', divHeight + 'px');
+	}
+	$('#taskList').append(
+		'<li style="margin-right: 0.58em" id=' +
+			data.task._id +
+			'>' +
+			data.task.name +
+			'</li>'
+	);
+	$('#' + data.task._id).after(
+		'<form class="deleteForm"> <input type="hidden" name="itemId" value=' +
+			data.task._id +
+			'> <button class="deleteButton"><i class="fa fa-times" style="font-size: 1.25em; "></i> </button> </form>'
+	);
 }
--- public/timerscript.js
+++ public/timerscript.js
@@ -1,88 +1,99 @@
-var workTimeLength = 45;
-var pauseTimeLength = 15;
+var workTimeLength = 60;
+var pauseTimeLength = 10;
 var interval;
 var workFlag; //if workFlag is true, pause timer will start after work timer is done
 
-window.onload = function() {
-	let timerLength = workTimeLength < 10 ? "0" + workTimeLength + ":00" : workTimeLength + ":00";
-	document.getElementById("timer").innerHTML = timerLength;
-}
+window.onload = function () {
+	let timerLength =
+		workTimeLength < 10 ? '0' + workTimeLength + ':00' : workTimeLength + ':00';
+	document.getElementById('timer').innerHTML = timerLength;
+};
 
 function startTimer(defaultTimer) {
-	document.getElementById("startButton").disabled = true;
+	document.getElementById('startButton').disabled = true;
 
 	let startTime = new Date().getTime();
 
-	interval = setInterval(function() {
+	interval = setInterval(function () {
 		let currentTime = new Date().getTime();
-		
+
 		let differenceTime = currentTime - startTime;
 
-		let minutesString = Math.floor((defaultTimer * 60 - differenceTime / 1000) / 60);
-		let secondsString = 60 - Math.floor((differenceTime % (60*1000)) / 1000);
+		let minutesString = Math.floor(
+			(defaultTimer * 60 - differenceTime / 1000) / 60
+		);
+		let secondsString = 60 - Math.floor((differenceTime % (60 * 1000)) / 1000);
 		//console.log(secondsString);
-		secondsString = secondsString === 60 ? "00" : secondsString < 10 ? "0" + secondsString : secondsString;
-		minutesString = secondsString === "00" ? minutesString + 1 : minutesString;
-		minutesString = minutesString < 10 ? "0" + minutesString: minutesString;
+		secondsString =
+			secondsString === 60
+				? '00'
+				: secondsString < 10
+				? '0' + secondsString
+				: secondsString;
+		minutesString = secondsString === '00' ? minutesString + 1 : minutesString;
+		minutesString = minutesString < 10 ? '0' + minutesString : minutesString;
 
-		document.getElementById("timer").innerHTML = minutesString + ":" + secondsString;
+		document.getElementById('timer').innerHTML =
+			minutesString + ':' + secondsString;
 
-		if(minutesString === "00" && secondsString === "00") {
-			var audio = new Audio("./sounds/piece-of-cake.mp3"); 
-            audio.play(); 
+		if (minutesString === '00' && secondsString === '00') {
+			var audio = new Audio('./sounds/piece-of-cake.mp3');
+			audio.play();
 			clearInterval(interval);
-			if(workFlag)
-				pauseTimer();
-			else
-				document.getElementById("startButton").disabled = false;
+			if (workFlag) pauseTimer();
+			else {
+				document.getElementById('startButton').disabled = false;
+				stopTimer();
+			}
 		}
 	}, 1000);
 }
 
 function stopTimer() {
 	clearInterval(interval);
-	document.getElementById("timer").innerHTML = "00:00";
-	document.getElementById("startButton").disabled = false;
-
+	document.getElementById('timer').innerHTML = '00:00';
+	document.getElementById('startButton').disabled = false;
 }
 
 function workTimer() {
 	workFlag = true;
-	document.body.style.backgroundColor = "coral";
+	document.body.style.backgroundColor = 'coral';
 	startTimer(workTimeLength);
 }
 
 function pauseTimer() {
 	workFlag = false;
-	document.body.style.backgroundColor = "#89CFF0";
+	document.body.style.backgroundColor = '#89CFF0';
 	startTimer(pauseTimeLength);
 }
 
 function openSettings() {
-	document.querySelector(".settingsTab").style.visibility = "visible";
-	document.querySelector(".settingsTab").style.opacity = 1;
+	document.querySelector('.settingsTab').style.visibility = 'visible';
+	document.querySelector('.settingsTab').style.opacity = 1;
 }
 
 function closeSettings() {
-	document.querySelector(".settingsTab").style.visibility ="hidden";
+	document.querySelector('.settingsTab').style.visibility = 'hidden';
 }
 
 function updateTime(id) {
 	let inputTime = document.getElementById(id).value;
 	inputTime = validateInputTime(inputTime, id);
 
-
-	if(id === "workTimeInput") {
+	if (id === 'workTimeInput') {
 		workTimeLength = inputTime;
-		document.getElementById("timer").innerHTML = workTimeLength < 10 ? "0" + workTimeLength + ":00" : workTimeLength + ":00";
-	}
-	else {
+		document.getElementById('timer').innerHTML =
+			workTimeLength < 10
+				? '0' + workTimeLength + ':00'
+				: workTimeLength + ':00';
+	} else {
 		pauseTimeLength = inputTime;
 	}
 }
 
 function validateInputTime(input, id) {
-	if(input === "") //if html input doesn't send anything because of wrong value ex: --5 send default value
-		return id === "workTimeInput" ? workTimeLength : pauseTimeLength; 
-	return input.replace(/[^0-9]/g,''); //strip string o any non-numeric character
+	if (input === '')
+		//if html input doesn't send anything because of wrong value ex: --5 send default value
+		return id === 'workTimeInput' ? workTimeLength : pauseTimeLength;
+	return input.replace(/[^0-9]/g, ''); //strip string o any non-numeric character
 }
